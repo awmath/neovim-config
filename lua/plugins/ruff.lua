e7b010dd6c9f60bd16356f14592e1a72e86a5e9b
Axel Wegener <axel.wegener@eneka.de>
2024-05-16 12:06:24 +0000

    add autoformat on save for ruff

--- lua/plugins/ruff.lua
+++ lua/plugins/ruff.lua
@@ -1,3 +1,18 @@
 return {
-  "astral-sh/ruff-lsp",
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {
+          -- somehow only keep typs hints...
+        },
+      },
+    },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+    },
+  },
 }
