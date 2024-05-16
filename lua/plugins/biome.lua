d80fab4c39b724a8a4ec46fa1fc4aa4159f78bdb
Axel Wegener <axel.wegener@eneka.de>
2024-05-16 10:10:34 +0000

    replace tsserver with biome

---
+++ lua/plugins/biome.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      --- other options
+      servers = {
+        tsserver = {
+          autostart = false,
+        },
+        biome = {},
+        -- other language servers
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      -- for language support
+      -- @see https://biomejs.dev/internals/language-support/
+      formatters_by_ft = {
+        ["javascript"] = { "biome" },
+        ["javascriptreact"] = { "biome" },
+        ["typescript"] = { "biome" },
+        ["typescriptreact"] = { "biome" },
+        ["json"] = { "biome" },
+        ["jsonc"] = { "biome" },
+        -- ["vue"] = { "biome" },
+        -- ["css"] = { "biome" },
+        -- ["scss"] = { "biome" },
+        -- ["less"] = { "biome" },
+        -- ["html"] = { "biome" },
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "biome",
+      },
+    },
+  },
+}
