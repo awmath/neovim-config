0b0cccdd1b32e27c5ad30d5256a9a810cf6e3986
Axel Wegener <axel.wegener@eneka.de>
2024-05-22 09:25:09 +0000

    fix doge statup install

--- lua/plugins/doge.lua
+++ lua/plugins/doge.lua
@@ -1,8 +1,10 @@
 return {
     "kkoomen/vim-doge",
     lazy = false,
-    run = ":call doge#install()",
     init = function()
         vim.g.doge_doc_standard_python = 'google';
+    end,
+    config = function()
+        vim.cmd(":call doge#install()");
     end
 }
